77147bdad2e772ffe266f353d5d41a83d8cd9c0f
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-08 11:59:22 +0530

    DynamicPages(values in parameters 2)

--- pages/news/[newsid].js
+++ pages/news/[newsid].js
@@ -1,5 +1,14 @@
 // Dynamic pages route - file name must be inclosed within square bracket ex-[newsid].js
+// To extract the concrete values entered in the url,when the page is loaded,Next js gives us a special hook which we can use.A special react hook used in functional component.
+
+import {useRouter} from 'next/router'; 
+// its react hook but customised by next team 
+
 const DetailPage = () => {
+    const router=useRouter();
+    const newsid=router.query.newsid;
+    console.log(newsid)
+    // newsid-its nothing but the dynamic page file name
   return (
     <h1>Detail Page</h1>
   )

6786a8a0a687b93297c9edfab9276cb80a1adf30
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-08 11:43:33 +0530

    DynamicPages(with parameters-1)

---
+++ pages/news/[newsid].js
@@ -0,0 +1,8 @@
+// Dynamic pages route - file name must be inclosed within square bracket ex-[newsid].js
+const DetailPage = () => {
+  return (
+    <h1>Detail Page</h1>
+  )
+}
+
+export default DetailPage

dc698735234c5c267639eea8a28f99f8f0879d92
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-09 10:47:44 +0530

    linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,16 @@
-const NewsPage = () => {
+import { Fragment } from "react"
+import Link from 'next/link';
+
+const NewsPage = () => {  
     return (
-      <div>Newspage</div>
+      <Fragment>
+        <h1>Newspage</h1>
+        <ul>
+          {/* <li><a href="news/next">Next Js News</a></li> */}
+          <li><Link href="news/next">Next Js News</Link></li>
+          <li>React Js News</li> 
+        </ul>
+      </Fragment>      
     )
   }
   
